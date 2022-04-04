3d92606f64a1ef19a03a3deda7c3b5b5228bfa79
Rashi <banerjee1909@gmail.com>
2022-04-04 16:25:10 +0530

    Initial Commit

---
+++ Dominos/ViewController.swift
@@ -0,0 +1,31 @@
+//
+//  ViewController.swift
+//  Dominos
+//
+//  Created by MAC 02 on 08/03/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    @IBOutlet var nameField: UITextField!
+    
+    @IBOutlet var usernameField: UITextField!
+    
+    @IBOutlet var passwordField: UITextField!
+
+//    @IBOutlet weak var labelResult: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        
+    }
+    
+    @IBAction func signUp(_ sender: Any) {
+    }
+    @IBAction func homePage(_ sender: Any) {
+    }
+}
+
